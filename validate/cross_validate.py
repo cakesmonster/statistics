85c511565d1282724a8647b9342275cf6d77d302
cakemonster <996165943@qq.com>
2022-06-06 09:37:28 +0800

    修改交叉验证方法

--- validate/cross_validate.py
+++ validate/cross_validate.py
@@ -1,48 +1,48 @@
 # coding: utf-8
+import copy
+
 from main import idw
-from main import load
+from main import load_points
 from util import statistics
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
-default_execl_index = 0
+default_sheet_index = 0
 default_has_title = True
 
 
 def cross_interpolation(points):
-    all_predict_points = []
+    predict_points = []
     for i in range(len(points)):
-        temp_measure_points = []
-        temp_predict_point = list.copy(points[i])
-        if i == 0:
-            temp_measure_points.extend(points[1:])
-        else:
-            temp_measure_points.extend(points[0: i - 1])
-            if i + 1 != len(points):
-                temp_measure_points.extend(points[i + 1:])
-
+        temp_measure_points = copy.deepcopy(points)
+        temp_predict_point = temp_measure_points.pop(i)
         z = idw.interpolation(temp_predict_point[0], temp_predict_point[1], temp_measure_points,
                               len(temp_measure_points))
-        # print('predict z : {}, measure z :{}'.format(z, temp_predict_point[2]))
         temp_predict_point[2] = z
-        all_predict_points.append(temp_predict_point)
+        predict_points.append(temp_predict_point)
 
-    return all_predict_points
+    return predict_points
 
 
-def process(path, lon_index, lat_index, z_index):
-    all_points = load.load_by_excel(path, default_execl_index, lon_index, lat_index, z_index, default_has_title)
+def process(path, sheet_index, lon_index, lat_index, z_index, has_title):
+    all_points = load_points.load_by_excel(path, sheet_index, lon_index, lat_index, z_index, has_title)
     predict_points = cross_interpolation(all_points)
 
     for i in range(len(all_points)):
-        print('predict z :{} ,   measure z : {}'.format(predict_points[i], all_points[i]))
+        print('predict point :{} ,   measure point : {}'.format(predict_points[i], all_points[i]))
 
     measure_zs = np.array(all_points)[..., 2]
     predict_zs = np.array(predict_points)[..., 2]
 
-    print('R2: {}'.format(r2_score(measure_zs, predict_zs)))
     print('ME: {}'.format(statistics.me(measure_zs, predict_zs)))
+    print('RMSE: {}'.format(mean_squared_error(measure_zs, predict_zs) ** .5))
+    print('R2: {}'.format(r2_score(measure_zs, predict_zs)))
 
 
 if __name__ == '__main__':
-    process("/Users/cakemonster/Desktop/statics/ace1.xls", 0, 2, 3)
+    excel_file = '/Users/cakemonster/Desktop/statics/ace1.xls'
+    _lon_index = 1
+    _lat_index = 2
+    _z_index = 3
+    process(excel_file, default_sheet_index, _lon_index, _lat_index, _z_index, default_has_title)
