51144b6d6bf9a01aee9cf05860c849d510967b20
cakemonster <996165943@qq.com>
2022-06-09 21:49:20 +0800

    修改地址列号可以作为参数传进来

--- util/analyzer_address.py
+++ util/analyzer_address.py
@@ -26,22 +26,26 @@ def analyzer_address(address):
     return location
 
 
-def process_line(row):
-    new_row = [row[0].value, row[1].value]
-    address = row[2].value
-    new_row.append(row[2].value)
+def process_line(row, address_index):
+    new_line = []
+    lon = ''
+    lat = ''
+    address = row[address_index].value.trim()
     if len(address) != 0 and address != '':
         location = analyzer_address(address)
         splits = location.split(',')
-        new_row.append(splits[0])
-        new_row.append(splits[1])
-    else:
-        new_row.append(row[3].value)
-        new_row.append(row[4].value)
-    return new_row
+        lon = splits[0]
+        lat = splits[1]
+
+    for col in row:
+        new_line.append(col.value)
+
+    new_line.append(lon)
+    new_line.append(lat)
+    return new_line
 
 
-def process(input_file, output_file, sheet_index):
+def process(input_file, output_file, sheet_index, address_index):
     if not os.path.exists(input_file):
         sys.exit('file: {} is not exist'.format(input_file))
     work_book = xlrd.open_workbook(input_file)
@@ -49,17 +53,18 @@ def process(input_file, output_file, sheet_index):
     new_lines = []
     try:
         for row_number in range(sheet.nrows):
-            new_line = process_line(sheet.row(row_number))
+            new_line = process_line(sheet.row(row_number), address_index)
             new_lines.append(new_line)
     except Exception:
         sys.exit('load points in file: {} error'.format(input_file))
 
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         write = csv.writer(f)
         write.writerows(new_lines)
 
 
 if __name__ == '__main__':
-    _input_file = '/Users/cakemonster/Desktop/fit_populations/test.xlsx'
-    _output_file = '/Users/cakemonster/Desktop/fit_populations/test_fill.csv'
-    process(_input_file, _output_file, default_sheet_index)
+    _input_file = '/Users/cakemonster/Desktop/fit_populations/周边居民地址(1).xlsx'
+    _output_file = '/Users/cakemonster/Desktop/fit_populations/周边居民地址(1)_fill.csv'
+    _address_index = 4
+    process(_input_file, _output_file, default_sheet_index, _address_index)
