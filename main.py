fc39c9f5c2f586e090772acdaf99505829e27a92
cakemonster <996165943@qq.com>
2022-11-05 23:01:16 +0800

    添加注释

--- main.py
+++ main.py
@@ -42,34 +42,58 @@ def load_fit_population(path):
 
 
 def load_fit_population1(path):
+    """
+    加载需要拟合的文件
+    :param path:  文件的路径
+    :return: 需要拟合的点
+    """
+    # 使用excel读取文件
     work_book = xlrd.open_workbook(path)
+    # 获取excel的第一个sheet
     sheet = work_book.sheet_by_index(0)
 
     title = []
     fit_points = []
+    # 遍历第一个sheet里的所有行
     for row_number in range(sheet.nrows):
         # 前两行是标题直接放title的列表里
         if row_number == 0:
             title.append(sheet.row(row_number))
             continue
+        # 第一列是姓名
         name = sheet.row(row_number)[0].value
+        # 第二列是地址
         address = sheet.row(row_number)[1].value
+        # 第三列是经度
         lon = sheet.row(row_number)[2].value
+        # 第四列是纬度
         lat = sheet.row(row_number)[3].value
+        # 构建一个点
         fit_point = [name, address, lon, lat]
         fit_points.append(fit_point)
     return title, fit_points
 
 
 def fit(points, fit_points):
+    """
+    拟合
+    :param points: 所有的点
+    :param fit_points: 需要拟合的点
+    :return:
+    """
     new_fit_points = []
+    # 遍历需要拟合的点
     for fit_point in fit_points:
         new_fit_point = []
         new_fit_point.extend(fit_point)
+        # 判断需要拟合的点的第三列和第四列不为空，也就是经纬度不为空
         if fit_point[2] != '' and fit_points[3] != '':
+            # 调用idw方法进行差值
             z = idw.interpolation(float(fit_point[2]), float(fit_point[3]), points, len(points))
+            # 新的点的数据就是长这样： [name, address, x , y, z]
             new_fit_point.append(z)
         else:
+            # 如果经纬度有一个是空的，就不计算z值，直接插入一个空字符串
             new_fit_point.append('')
         new_fit_points.append(new_fit_point)
 
@@ -77,6 +101,7 @@ def fit(points, fit_points):
     # new_path = os.path.join(new_dir, filename + '_new.csv')
     #
     # write_csv(new_path, new_fit_points)
+    # 返回所有拟合后的点
     return new_fit_points
 
 
@@ -89,10 +114,14 @@ def write_csv(path, points):
 if __name__ == '__main__':
     _dir = '/Users/cakemonster/Desktop/fit/'
     _new_dir = '/Users/cakemonster/Desktop/new_fit/'
-
-    title, fit_points = load_fit_population1('/Users/cakemonster/Desktop/fit/fillpeople(1).xlsx')
+    # 获取需要拟合的点，文件格式是第一列是姓名，第二列是地址，第三列是经度、第四列是纬度
+    # 已经写死了第三列、第四列必须是经纬度，所以如果你自己需要计算别的点的话就需要把经纬度放到第三列和第四列
+    title, fit_points = load_fit_population1('/Users/cakemonster/Desktop/fillpeople.xlsx')
+    # 遍历第四列到第20列
     for i in range(3, 19):
-        _points = load_points.load_by_csv('/Users/cakemonster/Desktop/statics/采样点.csv', 1, 2, i, True)
+        # 加载采样点里的第i列的[x, y ,z]的所有的点
+        _points = load_points.load_by_csv('/Users/cakemonster/Desktop/sample.csv', 1, 2, i, True)
+        # 拟合需要拟合的点
         fit_points = fit(_points, fit_points)
         if i == 18:
             write_csv('/Users/cakemonster/Desktop/new_fit/result.csv', fit_points)
--- main/load_points.py
+++ main/load_points.py
@@ -27,23 +27,40 @@ def load_by_excel(path, sheet_index, lon_index, lat_index, z_index, has_title):
 
 
 def load_by_csv(path, lon_index, lat_index, z_index, has_title):
+    """
+    从csv加载所有的点
+    :param path: 文件的地址
+    :param lon_index: 经度的索引 就是第几列 - 1
+    :param lat_index: 纬度的索引
+    :param z_index: z的索引
+    :param has_title: 有没有标题
+    :return: 所有的点
+    """
     if not os.path.exists(path):
         sys.exit('file: {} is not exist'.format(path))
     row_number = 0
     points = []
+    # 读取文件
     with open(path, 'r') as f:
         reader = csv.reader(f)
+        # 遍历每一行
         for line in reader:
+            # 判断有标题，并且当前行是第一行，直接跳过
             if has_title and row_number == 0:
                 row_number += 1
                 continue
             try:
+                # 判断经度、纬度、z值的列都不是空的
                 if line[lon_index] != '' and line[lat_index] != '' and line[z_index] != '':
+                    # 新建一个点，[x, y ,z]
                     new_line = [float(line[lon_index]), float(line[lat_index]), float(line[z_index])]
+                    # 放到返回的所有的点的集合里
                     points.append(new_line)
             except Exception as e:
                 # sys.exit('load points in file: {} error'.format(path))
+                # 如果读取文件当前行有异常情况，直接打印当前行
                 print(line)
+    # 返回从csv里读到的点 格式就是[x, y ,z]
     return points
 
 
